b8afbcff75d86e31359b1839b1840b2746846d94
kajal-dizayn <90452779+kaj082@users.noreply.github.com>
2024-08-30 11:10:50 +0530

    map added

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -1,23 +1,16 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
-import { GOOGLE_API } from "../../contants/RouterContant";
 
-const mapContainerStyle = {
-  width: "100%",
-  height: "800px",
-};
-
-const MyMapComponent = ({ center }) => {
+const MyMapComponent = () => {
   return (
-    <LoadScript googleMapsApiKey={GOOGLE_API}>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={10}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    <iframe
+      title="Appcom Infotech LLP Location"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.5161210847166!2d72.52494301511476!3d23.027561123589527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84cebe99335b%3A0xcc5fe35573e43727!2sAppcom%20Infotech%20LLP!5e0!3m2!1sen!2sin!4v1693412345678!5m2!1sen!2sin"
+      width="100%"
+      height="800" // Adjusted height for a larger map view
+      style={{ border: 0 }}
+      allowFullScreen={true}
+      loading="lazy"
+    ></iframe>
   );
 };
 
